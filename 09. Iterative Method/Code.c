0afa06a8ca475810e135375eee96f7d987bf2548
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-12-22 22:17:00 +0530

    Create Code.c

---
+++ 09. Iterative Method/Code.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <math.h>
+
+float polyFunc(float x)
+{
+    return 2 * x - (log(x) / log(10)) - 7;
+}
+
+float diffFunc(float x)
+{
+    return 1 / (2 * x * log(10));
+}
+
+float xForm(float x)
+{
+    return ((log(x) / log(10)) + 7) / 2;
+}
+
+int steps = 0;
+
+void calIterative(float x, float allowErr)
+{
+    x = xForm(x);
+    steps++;
+    printf("%d Iteration and The value of x is : %f\n", steps, x);
+    
+    if(fabs(polyFunc(x)) <= allowErr)
+        return;
+    
+    calIterative(x, allowErr);
+}
+
+int main()
+{
+    float x, allowErr;
+
+    printf("Enter the initial value : ");
+    scanf("%f", &x);
+
+    if(diffFunc(x) < 1)
+    {
+        printf("Input is satisfying the condition.\n");
+        printf("\nEnter the allowed Error : ");
+        scanf("%f", &allowErr);
+        printf("\n");
+        
+        calIterative(x, allowErr);
+    }
+    else
+        printf("Oops invalid input!!!");
+    
+    return 0;
+}
+
+
+
